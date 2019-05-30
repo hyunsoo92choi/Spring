a1436c27b73c68f309c4187d3ce2937c552698b8
hychoi <hychoi@ebay.com>
2019-05-30 16:20:45 +0900

    KuromojiRequestEntity 누락 커밋

---
+++ src/main/java/com/eBayJP/kuromoji/entity/request/KuromojiRequestEntity.java
@@ -0,0 +1,28 @@
+package com.eBayJP.kuromoji.entity.request;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * <pre>
+ * com.eBayJP.kuromoji.entity.request_KuromojiRequestEntity.java
+ * </pre>
+ * @date : 2019. 5. 30. 오후 2:40:28
+ * @author : hychoi
+ */
+@ApiModel
+@Data
+public class KuromojiRequestEntity implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6536757080348425840L;
+
+	@ApiModelProperty(required=true, value="texts [max 1MB]")
+	List<String> texts= new ArrayList<>();
+}
